db1504065cdd03d843c591c4e315dcf18f105c2e
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-10 13:45:49 -0700

    add PWA/ in sw.js v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 `use strict`;
-importScripts(`sw-toolbox.js`); 
+importScripts(`PWA/sw-toolbox.js`); 
 toolbox.precache(["PWA/index.html"]); 
 toolbox.router.get(`/images/*`, toolbox.cacheFirst); 
 toolbox.router.get(`/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
