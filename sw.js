4cb977e9fdbd3a6153c6dcbf61385520aaf21e2a
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-10 14:07:56 -0700

    add all url in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 `use strict`;
 importScripts(`https://24academy.ru/PWA/sw-toolbox.js`); 
 toolbox.precache(["https://24academy.ru/PWA/index.html"]); 
-toolbox.router.get(`/images/*`, toolbox.cacheFirst); 
-toolbox.router.get(`/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.router.get(`https://24academy.ru/PWA/static/media/*`, toolbox.cacheFirst); 
+toolbox.router.get(`https://24academy.ru/PWA/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
