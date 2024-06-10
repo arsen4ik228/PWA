bb9f4b1b13910077ff06ca50ef7c62207626afd2
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-10 16:46:05 -0700

    try notification v2 + new sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,27 @@
-`use strict`;
-importScripts(`https://24academy.ru/PWA/sw-toolbox.js`); 
-toolbox.precache(["https://24academy.ru/PWA/index.html"]); 
-toolbox.router.get(`https://24academy.ru/PWA/static/media/*`, toolbox.cacheFirst); 
-toolbox.router.get(`https://24academy.ru/PWA/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+// `use strict`;
+// importScripts(`https://24academy.ru/PWA/sw-toolbox.js`); 
+// toolbox.precache(["https://24academy.ru/PWA/index.html"]); 
+// toolbox.router.get(`https://24academy.ru/PWA/static/media/*`, toolbox.cacheFirst); 
+// toolbox.router.get(`https://24academy.ru/PWA/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
+    // Здесь можно выполнить асинхронные операции, например, очистить кэш
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+    // Здесь можно выполнить асинхронные операции, например, удалить устаревшие кэшированные ресурсы
+});
+
+self.addEventListener('push', function(event) {
+    const options = {
+        body: 'Это ваше уведомление',
+        icon: '/path/to/icon.png',
+        badge: '/path/to/badge.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification('Title of Notification', options)
+    );
+});
