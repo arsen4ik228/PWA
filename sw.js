48b6dd0af8194d1164f7c5bf74092653b572be23
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-10 18:41:41 -0700

    video 3 start config

--- sw.js
+++ sw.js
@@ -32,10 +32,36 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
 
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+}
 
 self.addEventListener("activate", async (e) => {
 
-    const subscription = await self.registration.pushManager.subscribe({})
+    const subscription = await self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array("BJpu-o7kq-OiFNc0HA-JUru2KD6jmvuIuKJ7VKkmTiFD5kr_9ScQ0V70O9DreSBJoXNI9P3pGL7_ssdYt4QLAJg")
+    })
     console.log(subscription)
 })
+
+
+
+
+// Public Key:
+// BJpu-o7kq-OiFNc0HA-JUru2KD6jmvuIuKJ7VKkmTiFD5kr_9ScQ0V70O9DreSBJoXNI9P3pGL7_ssdYt4QLAJg
+
+// Private Key:
+// _qzOFBUdFvMpVDZc4OsU39Nfgyi21-zntSYJg7RIbSs
