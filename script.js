55cfcdf22547d31f6fa18f77b97729ee547008f7
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-10 18:07:31 -0700

    2 video

--- script.js
+++ script.js
@@ -17,59 +17,57 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // В вашем основном JavaScript файле
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-    navigator.serviceWorker.register('/PWA/sw.js')
-    .then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope); 
-        return registration.pushManager;
-      })
-    .then(function(pushManager) {
-        if (pushManager) {
-            // Теперь мы уверены, что pushManager существует
-            return pushManager.requestPermission();
-        } else {
-            throw new Error('pushManager is not available');
-        }
-      })
-    .then(function(permission) {
-        if (permission === 'granted') {
-          console.log('Разрешено показывать уведомления');
-          // Здесь вы можете добавить логику для отправки уведомлений через Service Worker
-        } else {
-          console.log('Не разрешено показывать уведомления');
-        }
-      })
-    .catch(function(error) {
-        console.error('Ошибка при запросе разрешения на уведомления:', error);
-      });
-}
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const checkPermission = () => {
-//     if (!('service Worker' in navigator)) {
-//         throw new Error("No support for service worker!")
-//     }
-// }    
-
-// const registers = async () => {
-//     const registration = await navigator.serviceWorker.register('sw.js');
-//     return registration;
+// if ('serviceWorker' in navigator && 'PushManager' in window) {
+//     navigator.serviceWorker.register('/PWA/sw.js')
+//     .then(function(registration) {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope); 
+//         return registration.pushManager;
+//       })
+//     .then(function(pushManager) {
+//         if (pushManager) {
+//             // Теперь мы уверены, что pushManager существует
+//             return pushManager.requestPermission();
+//         } else {
+//             throw new Error('pushManager is not available');
+//         }
+//       })
+//     .then(function(permission) {
+//         if (permission === 'granted') {
+//           console.log('Разрешено показывать уведомления');
+//           // Здесь вы можете добавить логику для отправки уведомлений через Service Worker
+//         } else {
+//           console.log('Не разрешено показывать уведомления');
+//         }
+//       })
+//     .catch(function(error) {
+//         console.error('Ошибка при запросе разрешения на уведомления:', error);
+//       });
 // }
 
-// checkPermission()
-// registers()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const registers = async () => {
+    const registration = await navigator.serviceWorker.register('/PWA/sw.js');
+    return registration;
+}
+const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+    if (permission !== 'granted') {
+        throw new Error("Notification permission not granted")
+    };
+}
+const main = async () => {
+    checkPermission()
+    await requestNotificationPermission()
+    await registers()
+}
+
+main()
--- sw.js
+++ sw.js
@@ -4,24 +4,38 @@
 // toolbox.router.get(`https://24academy.ru/PWA/static/media/*`, toolbox.cacheFirst); 
 // toolbox.router.get(`https://24academy.ru/PWA/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
 
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installing.');
-    // Здесь можно выполнить асинхронные операции, например, очистить кэш
-});
 
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activating.');
-    // Здесь можно выполнить асинхронные операции, например, удалить устаревшие кэшированные ресурсы
-});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'Это ваше уведомление',
-        icon: '/path/to/icon.png',
-        badge: '/path/to/badge.png'
-    };
 
-    event.waitUntil(
-        self.registration.showNotification('Title of Notification', options)
-    );
-});
+// self.addEventListener('install', function(event) {
+//     console.log('Service Worker installing.');
+//     // Здесь можно выполнить асинхронные операции, например, очистить кэш
+// });
+
+// self.addEventListener('activate', function(event) {
+//     console.log('Service Worker activating.');
+//     // Здесь можно выполнить асинхронные операции, например, удалить устаревшие кэшированные ресурсы
+// });
+
+// self.addEventListener('push', function(event) {
+//     const options = {
+//         body: 'Это ваше уведомление',
+//         icon: '/path/to/icon.png',
+//         badge: '/path/to/badge.png'
+//     };
+
+//     event.waitUntil(
+//         self.registration.showNotification('Title of Notification', options)
+//     );
+// });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+self.addEventListener("activate", async (e) => {
+
+    const subscription = await self.registration.pushManager.subscribe({})
+    console.log(subscription)
+})
