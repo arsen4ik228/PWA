8068369d4eb0296299a1314699096a6be456116b
arsen4ik228 <belosheikin2003@yandex.ru>
2024-06-17 14:25:51 -0700

    new void subscribe

--- sw.js
+++ sw.js
@@ -1,57 +1,4 @@
-// `use strict`;
-// importScripts(`https://24academy.ru/PWA/sw-toolbox.js`); 
-// toolbox.precache(["https://24academy.ru/PWA/index.html"]); 
-// toolbox.router.get(`https://24academy.ru/PWA/static/media/*`, toolbox.cacheFirst); 
-// toolbox.router.get(`https://24academy.ru/PWA/*`, toolbox.networkFirst, { networkTimeoutSeconds: 5});
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// self.addEventListener('install', function(event) {
-//     console.log('Service Worker installing.');
-//     // Здесь можно выполнить асинхронные операции, например, очистить кэш
-// });
-
-// self.addEventListener('activate', function(event) {
-//     console.log('Service Worker activating.');
-//     // Здесь можно выполнить асинхронные операции, например, удалить устаревшие кэшированные ресурсы
-// });
-
-// self.addEventListener('push', function(event) {
-//     const options = {
-//         body: 'Это ваше уведомление',
-//         icon: '/path/to/icon.png',
-//         badge: '/path/to/badge.png'
-//     };
-
-//     event.waitUntil(
-//         self.registration.showNotification('Title of Notification', options)
-//     );
-// });
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-const urlBase64ToUint8Array = base64String => {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-
-    return outputArray;
-}
-
-let accountId; // Переменная для хранения accountId
-
+let accountId;
 self.addEventListener('message', event => {
     const { type, data } = event.data;
     
@@ -60,53 +7,136 @@ self.addEventListener('message', event => {
         accountId = data.accountId;
         console.log(`сообщение слушатель ${accountId}`);
         
-        self.clients.matchAll().then(clients => {
-            clients.forEach(client => client.postMessage({ type: 'ACCOUNT_ID_SET' }));
-        });
+        // Вызываем асинхронную функцию subscribeAndNotify
+        subscribeAndNotify();
     }
 });
 
-const saveSubscription = async (subscription) => {
-    console.log(`сохранение подписки${accountId}`);
+// Определяем асинхронную функцию
+async function subscribeAndNotify() {
+    try {
+        const subscription = await self.registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array("BJHxrYJR3WgpNrUYXpuAR6ZdIwTuC09dkzJH6Ca427K6Q1lEmqgFAQeNkvEwh8ZfAgyUbMJyD6FuJZqb_SX9WeE")
+        });
+
+        // Сохраняем подписку
+        const response = await saveSubscription(subscription);
+        console.log(response);
+
+        // Уведомляем клиентов
+        self.clients.matchAll().then(clients => {
+            clients.forEach(client => client.postMessage({ type: 'ACCOUNT_ID_SET' }));
+        });
+    } catch (error) {
+        console.error('Ошибка при подписке на push-уведомления:', error);
+    }
+}
+
+// Функция для преобразования строки в массив uint8array
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+       .replace(/\-/g, '+')
+       .replace(/_/g, '/');
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+};
+
+// Функция для сохранения подписки
+async function saveSubscription(subscription) {
+    console.log(`сохранение подписки ${accountId}`);
     
     const response = await fetch(`https://24academy.ru/api/${accountId}/save-subscription`, {
         method: 'post',
         headers: { 'Content-type': "application/json" },
         body: JSON.stringify(subscription)
-    })
+    });
 
-    return response.json()
+    return response.json();
 }
 
-self.addEventListener("activate", async (e) => {
-    const subscription = await self.registration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array("BJHxrYJR3WgpNrUYXpuAR6ZdIwTuC09dkzJH6Ca427K6Q1lEmqgFAQeNkvEwh8ZfAgyUbMJyD6FuJZqb_SX9WeE")
-    })
-
-    const response = await saveSubscription(subscription)
-    console.log(response)
-})
 
 
-self.addEventListener("push", e => {
-    // Преобразование текстового содержимого события в объект JSON
-    let pushData = {};
-    try {
-        pushData = JSON.parse(e.data.text());
-    } catch (error) {
-        console.error('Ошибка при разборе JSON:', error);
-        return; // Если JSON не может быть разобран, прекращаем выполнение
-    }
+// const urlBase64ToUint8Array = base64String => {
+//     const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+//     const base64 = (base64String + padding)
+//         .replace(/\-/g, '+')
+//         .replace(/_/g, '/');
 
-    // Проверяем, что объект содержит поля title и content
-    if (pushData.title && pushData.content) {
-        // Создаем уведомление с использованием только поля title
-        self.registration.showNotification(pushData.title, { body: pushData.content });
-    } else {
-        console.error('Полученные данные не содержат обязательных полей title и/or content');
-    }
-});
+//     const rawData = atob(base64);
+//     const outputArray = new Uint8Array(rawData.length);
+
+//     for (let i = 0; i < rawData.length; ++i) {
+//         outputArray[i] = rawData.charCodeAt(i);
+//     }
+
+//     return outputArray;
+// }
+
+// let accountId; // Переменная для хранения accountId
+
+// self.addEventListener('message', event => {
+//     const { type, data } = event.data;
+    
+//     if (type === 'SET_ACCOUNT_ID') {
+//         // Сохраняем accountId в переменную
+//         accountId = data.accountId;
+//         console.log(`сообщение слушатель ${accountId}`);
+        
+//         self.clients.matchAll().then(clients => {
+//             clients.forEach(client => client.postMessage({ type: 'ACCOUNT_ID_SET' }));
+//         });
+//     }
+// });
+
+// const saveSubscription = async (subscription) => {
+//     console.log(`сохранение подписки${accountId}`);
+    
+//     const response = await fetch(`https://24academy.ru/api/${accountId}/save-subscription`, {
+//         method: 'post',
+//         headers: { 'Content-type': "application/json" },
+//         body: JSON.stringify(subscription)
+//     })
+
+//     return response.json()
+// }
+
+// self.addEventListener("activate", async (e) => {
+//     const subscription = await self.registration.pushManager.subscribe({
+//         userVisibleOnly: true,
+//         applicationServerKey: urlBase64ToUint8Array("BJHxrYJR3WgpNrUYXpuAR6ZdIwTuC09dkzJH6Ca427K6Q1lEmqgFAQeNkvEwh8ZfAgyUbMJyD6FuJZqb_SX9WeE")
+//     })
+
+//     const response = await saveSubscription(subscription)
+//     console.log(response)
+// })
+
+
+// self.addEventListener("push", e => {
+//     // Преобразование текстового содержимого события в объект JSON
+//     let pushData = {};
+//     try {
+//         pushData = JSON.parse(e.data.text());
+//     } catch (error) {
+//         console.error('Ошибка при разборе JSON:', error);
+//         return; // Если JSON не может быть разобран, прекращаем выполнение
+//     }
+
+//     // Проверяем, что объект содержит поля title и content
+//     if (pushData.title && pushData.content) {
+//         // Создаем уведомление с использованием только поля title
+//         self.registration.showNotification(pushData.title, { body: pushData.content });
+//     } else {
+//         console.error('Полученные данные не содержат обязательных полей title и/or content');
+//     }
+// });
 
 
 
